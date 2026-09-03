rivet-dev/rivet

feat(http): stream HTTP bodies end-to-end

engine/sdks/rust/envoy-client/tests/support/actor_http_stream.rs
@@ -1,0 +1,301 @@
+use std::{
+	collections::HashMap,
+	sync::atomic::{AtomicUsize, Ordering},
+	sync::{Arc, Mutex},
+	time::Duration,
+};
+
+use tokio::sync::{mpsc, oneshot};
+use tokio::task::yield_now;
+
+use super::http::ActiveHttpRequestGuard;
+use super::tests::{
+	StreamingCallbacks, TestCallbacks, actor_config, build_shared_context, message_id,
+	recv_ws_tunnel_msg, request_start, wait_for_stopped_event, wait_for_zero,
+};
+use super::{ToActor, create_actor, protocol};
+use crate::{
+	async_counter::AsyncCounter,
+	context::SharedActorEntry,
+	handle::EnvoyHandle,
+	http::{HTTP_BODY_MAX_CHUNK_SIZE, HTTP_BODY_STREAM_CHANNEL_CAPACITY, ResponseChunk},
+};
+
+#[tokio::test]
+async fn streamed_request_backpressure_does_not_block_actor_control_loop() {
+	let (fetch_started_tx, fetch_started_rx) = oneshot::channel();
+	let (fetch_dropped_tx, fetch_dropped_rx) = oneshot::channel();
+	let callbacks = Arc::new(TestCallbacks::hanging(fetch_started_tx, fetch_dropped_tx));
+	let (shared, mut envoy_rx) = build_shared_context(callbacks);
+	let (ws_tx, mut ws_rx) = mpsc::unbounded_channel();
+	*shared.ws_tx.lock().await = Some(ws_tx);
+	let (actor_tx, _) = create_actor(
+		shared,
+		"actor-streamed-request".to_string(),
+		1,
+		actor_config(),
+		Vec::new(),
+		None,
+	);
+	let mut request = request_start();
+	request.method = "POST".to_owned();
+	request.stream = true;
+
+	actor_tx
+		.send(ToActor::ReqStart {
+			message_id: message_id(),
+			req: request,
+		})
+		.expect("failed to send request start");
+	fetch_started_rx
+		.await
+		.expect("fetch start sender dropped before request began");
+
+	for index in 0..=HTTP_BODY_STREAM_CHANNEL_CAPACITY {
+		let mut chunk_message_id = message_id();
+		chunk_message_id.message_index = index as u16 + 1;
+		actor_tx
+			.send(ToActor::ReqChunk {
+				message_id: chunk_message_id,
+				chunk: protocol::ToEnvoyRequestChunk {
+					body: vec![index as u8],
+					finish: false,
+				},
+			})
+			.expect("failed to send streamed request chunk");
+	}
+
+	tokio::time::timeout(Duration::from_secs(2), fetch_dropped_rx)
+		.await
+		.expect("overloaded request did not cancel its handler")
+		.expect("fetch drop sender dropped");
+	let abort = recv_ws_tunnel_msg(&mut ws_rx).await;
+	assert!(matches!(
+		abort.message_kind,
+		protocol::ToRivetTunnelMessageKind::ToRivetResponseAbort(protocol::ToRivetResponseAbort {
+			reason: protocol::HttpStreamAbortReason {
+				kind: protocol::HttpStreamAbortReasonKind::Overloaded,
+				..
+			}
+		})
+	));
+
+	actor_tx
+		.send(ToActor::Stop {
+			command_idx: 1,
+			reason: protocol::StopActorReason::StopIntent,
+		})
+		.expect("failed to send stop after request overload");
+	wait_for_stopped_event(&mut envoy_rx).await;
+}
+
+#[tokio::test]
+async fn streamed_request_remains_cancellable_after_upload_finishes() {
+	let (fetch_started_tx, fetch_started_rx) = oneshot::channel();
+	let (fetch_dropped_tx, fetch_dropped_rx) = oneshot::channel();
+	let callbacks = Arc::new(TestCallbacks::hanging(fetch_started_tx, fetch_dropped_tx));
+	let (shared, _envoy_rx) = build_shared_context(callbacks);
+	let (actor_tx, _) = create_actor(
+		shared,
+		"actor-finished-upload".to_string(),
+		1,
+		actor_config(),
+		Vec::new(),
+		None,
+	);
+	let mut request = request_start();
+	request.method = "POST".to_owned();
+	request.stream = true;
+
+	actor_tx
+		.send(ToActor::ReqStart {
+			message_id: message_id(),
+			req: request,
+		})
+		.expect("failed to send request start");
+	fetch_started_rx
+		.await
+		.expect("fetch start sender dropped before request began");
+	actor_tx
+		.send(ToActor::ReqChunk {
+			message_id: message_id(),
+			chunk: protocol::ToEnvoyRequestChunk {
+				body: Vec::new(),
+				finish: true,
+			},
+		})
+		.expect("failed to finish upload");
+	actor_tx
+		.send(ToActor::ReqAbort {
+			message_id: message_id(),
+			reason: protocol::HttpStreamAbortReason {
+				kind: protocol::HttpStreamAbortReasonKind::ClientDisconnect,
+				detail: None,
+			},
+		})
+		.expect("failed to abort completed upload");
+
+	tokio::time::timeout(Duration::from_secs(2), fetch_dropped_rx)
+		.await
+		.expect("request handler was not cancelled after upload completion")
+		.expect("fetch drop sender dropped");
+}
+
+#[tokio::test]
+async fn active_http_request_count_spans_streaming_response_drain() {
+	let (body_tx_tx, body_tx_rx) = oneshot::channel();
+	let callbacks = Arc::new(StreamingCallbacks {
+		body_tx: Mutex::new(Some(body_tx_tx)),
+	});
+	let (shared, _envoy_rx) = build_shared_context(callbacks);
+	let (ws_tx, mut ws_rx) = mpsc::unbounded_channel();
+	*shared.ws_tx.lock().await = Some(ws_tx);
+	let (actor_tx, active_http_request_count) = create_actor(
+		shared,
+		"actor-stream".to_string(),
+		1,
+		actor_config(),
+		Vec::new(),
+		None,
+	);
+
+	actor_tx
+		.send(ToActor::ReqStart {
+			message_id: message_id(),
+			req: request_start(),
+		})
+		.expect("failed to send request start");
+
+	let body_tx = tokio::time::timeout(Duration::from_secs(2), body_tx_rx)
+		.await
+		.expect("timed out waiting for response body sender")
+		.expect("response body sender dropped");
+	let start_msg = recv_ws_tunnel_msg(&mut ws_rx).await;
+	assert!(matches!(
+		start_msg.message_kind,
+		protocol::ToRivetTunnelMessageKind::ToRivetResponseStart(protocol::ToRivetResponseStart {
+			stream: true,
+			..
+		})
+	));
+	assert_eq!(active_http_request_count.load(), 1);
+
+	body_tx
+		.send(ResponseChunk::Data {
+			data: vec![7; HTTP_BODY_MAX_CHUNK_SIZE + 3],
+			finish: false,
+		})
+		.await
+		.expect("failed to send response data");
+
+	let first = recv_ws_tunnel_msg(&mut ws_rx).await;
+	let second = recv_ws_tunnel_msg(&mut ws_rx).await;
+	match first.message_kind {
+		protocol::ToRivetTunnelMessageKind::ToRivetResponseChunk(chunk) => {
+			assert_eq!(first.message_id.message_index, 1);
+			assert_eq!(chunk.body.len(), HTTP_BODY_MAX_CHUNK_SIZE);
+			assert!(!chunk.finish);
+		}
+		other => panic!("expected first response chunk, got {other:?}"),
+	}
+	match second.message_kind {
+		protocol::ToRivetTunnelMessageKind::ToRivetResponseChunk(chunk) => {
+			assert_eq!(second.message_id.message_index, 2);
+			assert_eq!(chunk.body.len(), 3);
+			assert!(!chunk.finish);
+		}
+		other => panic!("expected second response chunk, got {other:?}"),
+	}
+	assert_eq!(active_http_request_count.load(), 1);
+
+	body_tx
+		.send(ResponseChunk::Data {
+			data: Vec::new(),
+			finish: true,
+		})
+		.await
+		.expect("failed to finish response stream");
+	let finish = recv_ws_tunnel_msg(&mut ws_rx).await;
+	match finish.message_kind {
+		protocol::ToRivetTunnelMessageKind::ToRivetResponseChunk(chunk) => {
+			assert_eq!(finish.message_id.message_index, 3);
+			assert!(chunk.body.is_empty());
+			assert!(chunk.finish);
+		}
+		other => panic!("expected finish response chunk, got {other:?}"),
+	}
+
+	wait_for_zero(&active_http_request_count).await;
+}
+
+#[tokio::test]
+async fn http_request_guard_counter_is_visible_through_envoy_handle() {
+	let (shared, _envoy_rx) = build_shared_context(Arc::new(TestCallbacks::idle()));
+	let handle = EnvoyHandle {
+		shared: shared.clone(),
+		started_rx: tokio::sync::watch::channel(()).1,
+	};
+	let counter = Arc::new(AsyncCounter::new());
+	shared
+		.actors
+		.lock()
+		.expect("shared actor registry poisoned")
+		.entry("actor-4".to_string())
+		.or_insert_with(HashMap::new)
+		.insert(
+			4,
+			SharedActorEntry {
+				handle: mpsc::unbounded_channel().0,
+				active_http_request_count: counter.clone(),
+			},
+		);
+
+	let request_guard = ActiveHttpRequestGuard::new(counter);
+	let handle_counter = handle
+		.http_request_counter("actor-4", Some(4))
+		.expect("counter should be returned");
+	assert_eq!(handle_counter.load(), 1);
+
+	drop(request_guard);
+	assert_eq!(handle_counter.load(), 0);
+	assert!(
+		handle_counter
+			.wait_zero(crate::time::Instant::now() + Duration::from_secs(2))
+			.await
+	);
+}
+
+#[tokio::test]
+async fn active_http_request_counter_waiter_wakes_only_after_final_drop() {
+	let counter = Arc::new(AsyncCounter::new());
+	let guard_a = ActiveHttpRequestGuard::new(counter.clone());
+	let guard_b = ActiveHttpRequestGuard::new(counter.clone());
+	let wake_count = Arc::new(AtomicUsize::new(0));
+
+	let waiter = tokio::spawn({
+		let counter = counter.clone();
+		let wake_count = wake_count.clone();
+		async move {
+			let woke = counter
+				.wait_zero(crate::time::Instant::now() + Duration::from_secs(2))
+				.await;
+			if woke {
+				wake_count.fetch_add(1, Ordering::SeqCst);
+			}
+			woke
+		}
+	});
+
+	yield_now().await;
+	drop(guard_a);
+	yield_now().await;
+	assert_eq!(wake_count.load(Ordering::SeqCst), 0);
+	assert!(
+		!waiter.is_finished(),
+		"waiter should stay pending until the final in-flight request completes"
+	);
+
+	drop(guard_b);
+	assert!(waiter.await.expect("waiter should join"));
+	assert_eq!(wake_count.load(Ordering::SeqCst), 1);
+}

engine/sdks/rust/envoy-protocol/tests/remote_sql_compat.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use rivet_envoy_protocol::{
-	generated::{v4, v6},
+	generated::{v4, v7},
 	versioned::{
 		ProtocolCompatibilityDirection, ProtocolCompatibilityError, ProtocolCompatibilityFeature,
 		ToEnvoy, ToRivet,
@@ -8,10 +8,10 @@ use rivet_envoy_protocol::{
 };
 use vbare::OwnedVersionedData;
 
-fn remote_sql_request_exec() -> v6::ToRivet {
-	v6::ToRivet::ToRivetSqliteExecRequest(v6::ToRivetSqliteExecRequest {
+fn remote_sql_request_exec() -> v7::ToRivet {
+	v7::ToRivet::ToRivetSqliteExecRequest(v7::ToRivetSqliteExecRequest {
 		request_id: 1,
-		data: v6::SqliteExecRequest {
+		data: v7::SqliteExecRequest {
 			namespace_id: "namespace".into(),
 			actor_id: "actor".into(),
 			generation: 7,
@@ -20,25 +20,25 @@ fn remote_sql_request_exec() -> v6::ToRivet {
 	})
 }
 
-fn remote_sql_request_execute() -> v6::ToRivet {
-	v6::ToRivet::ToRivetSqliteExecuteRequest(v6::ToRivetSqliteExecuteRequest {
+fn remote_sql_request_execute() -> v7::ToRivet {
+	v7::ToRivet::ToRivetSqliteExecuteRequest(v7::ToRivetSqliteExecuteRequest {
 		request_id: 2,
-		data: v6::SqliteExecuteRequest {
+		data: v7::SqliteExecuteRequest {
 			namespace_id: "namespace".into(),
 			actor_id: "actor".into(),
 			generation: 7,
 			sql: "select ?".into(),
-			params: Some(vec![v6::SqliteBindParam::SqliteValueInteger(
-				v6::SqliteValueInteger { value: 1 },
+			params: Some(vec![v7::SqliteBindParam::SqliteValueInteger(
+				v7::SqliteValueInteger { value: 1 },
 			)]),
 		},
 	})
 }
 
-fn remote_sql_response_exec() -> v6::ToEnvoy {
-	v6::ToEnvoy::ToEnvoySqliteExecResponse(v6::ToEnvoySqliteExecResponse {
+fn remote_sql_response_exec() -> v7::ToEnvoy {
+	v7::ToEnvoy::ToEnvoySqliteExecResponse(v7::ToEnvoySqliteExecResponse {
 		request_id: 1,
-		data: v6::SqliteExecResponse::SqliteErrorResponse(v6::SqliteErrorResponse {
+		data: v7::SqliteExecResponse::SqliteErrorResponse(v7::SqliteErrorResponse {
 			group: "sqlite".into(),
 			code: "remote_unavailable".into(),
 			message: "remote sql execution is unavailable".into(),
@@ -46,10 +46,10 @@ fn remote_sql_response_exec() -> v6::ToEnvoy {
 	})
 }
 
-fn remote_sql_response_execute() -> v6::ToEnvoy {
-	v6::ToEnvoy::ToEnvoySqliteExecuteResponse(v6::ToEnvoySqliteExecuteResponse {
+fn remote_sql_response_execute() -> v7::ToEnvoy {
+	v7::ToEnvoy::ToEnvoySqliteExecuteResponse(v7::ToEnvoySqliteExecuteResponse {
 		request_id: 2,
-		data: v6::SqliteExecuteResponse::SqliteErrorResponse(v6::SqliteErrorResponse {
+		data: v7::SqliteExecuteResponse::SqliteErrorResponse(v7::SqliteErrorResponse {
 			group: "sqlite".into(),
 			code: "remote_unavailable".into(),
 			message: "remote sql execution is unavailable".into(),
@@ -57,27 +57,27 @@ fn remote_sql_response_execute() -> v6::ToEnvoy {
 	})
 }
 
-fn remote_sql_request_execute_batch() -> v6::ToRivet {
-	v6::ToRivet::ToRivetSqliteExecuteBatchRequest(v6::ToRivetSqliteExecuteBatchRequest {
+fn remote_sql_request_execute_batch() -> v7::ToRivet {
+	v7::ToRivet::ToRivetSqliteExecuteBatchRequest(v7::ToRivetSqliteExecuteBatchRequest {
 		request_id: 3,
-		data: v6::SqliteExecuteBatchRequest {
+		data: v7::SqliteExecuteBatchRequest {
 			namespace_id: "namespace".into(),
 			actor_id: "actor".into(),
 			generation: 7,
-			statements: vec![v6::SqliteBatchStatement {
+			statements: vec![v7::SqliteBatchStatement {
 				sql: "insert into t values (?)".into(),
-				params: Some(vec![v6::SqliteBindParam::SqliteValueInteger(
-					v6::SqliteValueInteger { value: 1 },
+				params: Some(vec![v7::SqliteBindParam::SqliteValueInteger(
+					v7::SqliteValueInteger { value: 1 },
 				)]),
 			}],
 		},
 	})
 }
 
-fn remote_sql_response_execute_batch() -> v6::ToEnvoy {
-	v6::ToEnvoy::ToEnvoySqliteExecuteBatchResponse(v6::ToEnvoySqliteExecuteBatchResponse {
+fn remote_sql_response_execute_batch() -> v7::ToEnvoy {
+	v7::ToEnvoy::ToEnvoySqliteExecuteBatchResponse(v7::ToEnvoySqliteExecuteBatchResponse {
 		request_id: 3,
-		data: v6::SqliteExecuteBatchResponse::SqliteExecuteBatchOk(v6::SqliteExecuteBatchOk {
+		data: v7::SqliteExecuteBatchResponse::SqliteExecuteBatchOk(v7::SqliteExecuteBatchOk {
 			results: Vec::new(),
 		}),
 	})
@@ -139,11 +139,11 @@ fn new_core_new_pegboard_envoy_allows_remote_sql_both_directions() -> Result<()>
 
 	assert!(matches!(
 		ToRivet::deserialize(&request, 4)?,
-		v6::ToRivet::ToRivetSqliteExecRequest(_)
+		v7::ToRivet::ToRivetSqliteExecRequest(_)
 	));
 	assert!(matches!(
 		ToEnvoy::deserialize(&response, 4)?,
-		v6::ToEnvoy::ToEnvoySqliteExecResponse(_)
+		v7::ToEnvoy::ToEnvoySqliteExecResponse(_)
 	));
 
 	Ok(())
@@ -235,11 +235,11 @@ fn remote_sql_batch_requires_v6() -> Result<()> {
 	let response = ToEnvoy::wrap_latest(remote_sql_response_execute_batch()).serialize(6)?;
 	assert!(matches!(
 		ToRivet::deserialize(&request, 6)?,
-		v6::ToRivet::ToRivetSqliteExecuteBatchRequest(_)
+		v7::ToRivet::ToRivetSqliteExecuteBatchRequest(_)
 	));
 	assert!(matches!(
 		ToEnvoy::deserialize(&response, 6)?,
-		v6::ToEnvoy::ToEnvoySqliteExecuteBatchResponse(_)
+		v7::ToEnvoy::ToEnvoySqliteExecuteBatchResponse(_)
 	));
 	Ok(())
 }

rivet-dev/actors

engine/sdks/rust/envoy-client/tests/support/actor_http_stream.rs
@@ -1,0 +1,269 @@
+use std::{
+	collections::HashMap,
+	sync::atomic::{AtomicUsize, Ordering},
+	sync::{Arc, Mutex},
+	time::Duration,
+};
+
+use tokio::sync::{mpsc, oneshot};
+use tokio::task::yield_now;
+
+use super::http::ActiveHttpRequestGuard;
+use super::tests::{
+	StreamingCallbacks, TestCallbacks, actor_config, build_shared_context, message_id,
+	recv_ws_tunnel_msg, request_start, wait_for_zero,
+};
+use super::{ToActor, create_actor, protocol};
+use crate::{
+	async_counter::AsyncCounter,
+	context::SharedActorEntry,
+	handle::EnvoyHandle,
+	http::{HTTP_BODY_MAX_CHUNK_SIZE, ResponseChunk},
+};
+
+#[tokio::test]
+async fn request_chunk_without_start_is_rejected_instead_of_buffered() {
+	let (shared, _envoy_rx) = build_shared_context(Arc::new(TestCallbacks::idle()));
+	let (ws_tx, mut ws_rx) = mpsc::unbounded_channel();
+	*shared.ws_tx.lock().await = Some(ws_tx);
+	let (actor_tx, _) = create_actor(
+		shared,
+		"actor-missing-request-start".to_string(),
+		1,
+		actor_config(),
+		Vec::new(),
+		None,
+	);
+
+	actor_tx
+		.send(ToActor::ReqChunk {
+			message_id: message_id(),
+			chunk: protocol::ToEnvoyRequestChunk {
+				body: vec![1, 2, 3],
+				finish: false,
+			},
+		})
+		.expect("failed to send request chunk");
+
+	let abort = recv_ws_tunnel_msg(&mut ws_rx).await;
+	assert!(matches!(
+		abort.message_kind,
+		protocol::ToRivetTunnelMessageKind::ToRivetResponseAbort(protocol::ToRivetResponseAbort {
+			reason: protocol::HttpStreamAbortReason {
+				kind: protocol::HttpStreamAbortReasonKind::HandlerError,
+				..
+			}
+		})
+	));
+}
+
+#[tokio::test]
+async fn streamed_request_remains_cancellable_after_upload_finishes() {
+	let (fetch_started_tx, fetch_started_rx) = oneshot::channel();
+	let (fetch_dropped_tx, fetch_dropped_rx) = oneshot::channel();
+	let callbacks = Arc::new(TestCallbacks::hanging(fetch_started_tx, fetch_dropped_tx));
+	let (shared, _envoy_rx) = build_shared_context(callbacks);
+	let (actor_tx, _) = create_actor(
+		shared,
+		"actor-finished-upload".to_string(),
+		1,
+		actor_config(),
+		Vec::new(),
+		None,
+	);
+	let mut request = request_start();
+	request.method = "POST".to_owned();
+	request.stream = true;
+
+	actor_tx
+		.send(ToActor::ReqStart {
+			message_id: message_id(),
+			req: request,
+		})
+		.expect("failed to send request start");
+	fetch_started_rx
+		.await
+		.expect("fetch start sender dropped before request began");
+	actor_tx
+		.send(ToActor::ReqChunk {
+			message_id: message_id(),
+			chunk: protocol::ToEnvoyRequestChunk {
+				body: Vec::new(),
+				finish: true,
+			},
+		})
+		.expect("failed to finish upload");
+	actor_tx
+		.send(ToActor::ReqAbort {
+			message_id: message_id(),
+			reason: protocol::HttpStreamAbortReason {
+				kind: protocol::HttpStreamAbortReasonKind::ClientDisconnect,
+				detail: None,
+			},
+		})
+		.expect("failed to abort completed upload");
+
+	tokio::time::timeout(Duration::from_secs(2), fetch_dropped_rx)
+		.await
+		.expect("request handler was not cancelled after upload completion")
+		.expect("fetch drop sender dropped");
+}
+
+#[tokio::test]
+async fn active_http_request_count_spans_streaming_response_drain() {
+	let (body_tx_tx, body_tx_rx) = oneshot::channel();
+	let callbacks = Arc::new(StreamingCallbacks {
+		body_tx: Mutex::new(Some(body_tx_tx)),
+	});
+	let (shared, _envoy_rx) = build_shared_context(callbacks);
+	let (ws_tx, mut ws_rx) = mpsc::unbounded_channel();
+	*shared.ws_tx.lock().await = Some(ws_tx);
+	let (actor_tx, active_http_request_count) = create_actor(
+		shared,
+		"actor-stream".to_string(),
+		1,
+		actor_config(),
+		Vec::new(),
+		None,
+	);
+
+	actor_tx
+		.send(ToActor::ReqStart {
+			message_id: message_id(),
+			req: request_start(),
+		})
+		.expect("failed to send request start");
+
+	let body_tx = tokio::time::timeout(Duration::from_secs(2), body_tx_rx)
+		.await
+		.expect("timed out waiting for response body sender")
+		.expect("response body sender dropped");
+	let start_msg = recv_ws_tunnel_msg(&mut ws_rx).await;
+	assert!(matches!(
+		start_msg.message_kind,
+		protocol::ToRivetTunnelMessageKind::ToRivetResponseStart(protocol::ToRivetResponseStart {
+			stream: true,
+			..
+		})
+	));
+	assert_eq!(active_http_request_count.load(), 1);
+
+	body_tx
+		.send(ResponseChunk::Data {
+			data: vec![7; HTTP_BODY_MAX_CHUNK_SIZE + 3],
+			finish: false,
+		})
+		.await
+		.expect("failed to send response data");
+
+	let first = recv_ws_tunnel_msg(&mut ws_rx).await;
+	let second = recv_ws_tunnel_msg(&mut ws_rx).await;
+	match first.message_kind {
+		protocol::ToRivetTunnelMessageKind::ToRivetResponseChunk(chunk) => {
+			assert_eq!(first.message_id.message_index, 1);
+			assert_eq!(chunk.body.len(), HTTP_BODY_MAX_CHUNK_SIZE);
+			assert!(!chunk.finish);
+		}
+		other => panic!("expected first response chunk, got {other:?}"),
+	}
+	match second.message_kind {
+		protocol::ToRivetTunnelMessageKind::ToRivetResponseChunk(chunk) => {
+			assert_eq!(second.message_id.message_index, 2);
+			assert_eq!(chunk.body.len(), 3);
+			assert!(!chunk.finish);
+		}
+		other => panic!("expected second response chunk, got {other:?}"),
+	}
+	assert_eq!(active_http_request_count.load(), 1);
+
+	body_tx
+		.send(ResponseChunk::Data {
+			data: Vec::new(),
+			finish: true,
+		})
+		.await
+		.expect("failed to finish response stream");
+	let finish = recv_ws_tunnel_msg(&mut ws_rx).await;
+	match finish.message_kind {
+		protocol::ToRivetTunnelMessageKind::ToRivetResponseChunk(chunk) => {
+			assert_eq!(finish.message_id.message_index, 3);
+			assert!(chunk.body.is_empty());
+			assert!(chunk.finish);
+		}
+		other => panic!("expected finish response chunk, got {other:?}"),
+	}
+
+	wait_for_zero(&active_http_request_count).await;
+}
+
+#[tokio::test]
+async fn http_request_guard_counter_is_visible_through_envoy_handle() {
+	let (shared, _envoy_rx) = build_shared_context(Arc::new(TestCallbacks::idle()));
+	let handle = EnvoyHandle {
+		shared: shared.clone(),
+		started_rx: tokio::sync::watch::channel(()).1,
+	};
+	let counter = Arc::new(AsyncCounter::new());
+	shared
+		.actors
+		.lock()
+		.expect("shared actor registry poisoned")
+		.entry("actor-4".to_string())
+		.or_insert_with(HashMap::new)
+		.insert(
+			4,
+			SharedActorEntry {
+				handle: mpsc::unbounded_channel().0,
+				active_http_request_count: counter.clone(),
+			},
+		);
+
+	let request_guard = ActiveHttpRequestGuard::new(counter);
+	let handle_counter = handle
+		.http_request_counter("actor-4", Some(4))
+		.expect("counter should be returned");
+	assert_eq!(handle_counter.load(), 1);
+
+	drop(request_guard);
+	assert_eq!(handle_counter.load(), 0);
+	assert!(
+		handle_counter
+			.wait_zero(crate::time::Instant::now() + Duration::from_secs(2))
+			.await
+	);
+}
+
+#[tokio::test]
+async fn active_http_request_counter_waiter_wakes_only_after_final_drop() {
+	let counter = Arc::new(AsyncCounter::new());
+	let guard_a = ActiveHttpRequestGuard::new(counter.clone());
+	let guard_b = ActiveHttpRequestGuard::new(counter.clone());
+	let wake_count = Arc::new(AtomicUsize::new(0));
+
+	let waiter = tokio::spawn({
+		let counter = counter.clone();
+		let wake_count = wake_count.clone();
+		async move {
+			let woke = counter
+				.wait_zero(crate::time::Instant::now() + Duration::from_secs(2))
+				.await;
+			if woke {
+				wake_count.fetch_add(1, Ordering::SeqCst);
+			}
+			woke
+		}
+	});
+
+	yield_now().await;
+	drop(guard_a);
+	yield_now().await;
+	assert_eq!(wake_count.load(Ordering::SeqCst), 0);
+	assert!(
+		!waiter.is_finished(),
+		"waiter should stay pending until the final in-flight request completes"
+	);
+
+	drop(guard_b);
+	assert!(waiter.await.expect("waiter should join"));
+	assert_eq!(wake_count.load(Ordering::SeqCst), 1);
+}

engine/sdks/rust/envoy-protocol/tests/remote_sql_compat.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use rivet_envoy_protocol::{
-	generated::{v4, v6},
+	generated::{v4, v8},
 	versioned::{
 		ProtocolCompatibilityDirection, ProtocolCompatibilityError, ProtocolCompatibilityFeature,
 		ToEnvoy, ToRivet,
@@ -8,10 +8,10 @@ use rivet_envoy_protocol::{
 };
 use vbare::OwnedVersionedData;
 
-fn remote_sql_request_exec() -> v6::ToRivet {
-	v6::ToRivet::ToRivetSqliteExecRequest(v6::ToRivetSqliteExecRequest {
+fn remote_sql_request_exec() -> v8::ToRivet {
+	v8::ToRivet::ToRivetSqliteExecRequest(v8::ToRivetSqliteExecRequest {
 		request_id: 1,
-		data: v6::SqliteExecRequest {
+		data: v8::SqliteExecRequest {
 			namespace_id: "namespace".into(),
 			actor_id: "actor".into(),
 			generation: 7,
@@ -20,25 +20,25 @@ fn remote_sql_request_exec() -> v6::ToRivet {
 	})
 }
 
-fn remote_sql_request_execute() -> v6::ToRivet {
-	v6::ToRivet::ToRivetSqliteExecuteRequest(v6::ToRivetSqliteExecuteRequest {
+fn remote_sql_request_execute() -> v8::ToRivet {
+	v8::ToRivet::ToRivetSqliteExecuteRequest(v8::ToRivetSqliteExecuteRequest {
 		request_id: 2,
-		data: v6::SqliteExecuteRequest {
+		data: v8::SqliteExecuteRequest {
 			namespace_id: "namespace".into(),
 			actor_id: "actor".into(),
 			generation: 7,
 			sql: "select ?".into(),
-			params: Some(vec![v6::SqliteBindParam::SqliteValueInteger(
-				v6::SqliteValueInteger { value: 1 },
+			params: Some(vec![v8::SqliteBindParam::SqliteValueInteger(
+				v8::SqliteValueInteger { value: 1 },
 			)]),
 		},
 	})
 }
 
-fn remote_sql_response_exec() -> v6::ToEnvoy {
-	v6::ToEnvoy::ToEnvoySqliteExecResponse(v6::ToEnvoySqliteExecResponse {
+fn remote_sql_response_exec() -> v8::ToEnvoy {
+	v8::ToEnvoy::ToEnvoySqliteExecResponse(v8::ToEnvoySqliteExecResponse {
 		request_id: 1,
-		data: v6::SqliteExecResponse::SqliteErrorResponse(v6::SqliteErrorResponse {
+		data: v8::SqliteExecResponse::SqliteErrorResponse(v8::SqliteErrorResponse {
 			group: "sqlite".into(),
 			code: "remote_unavailable".into(),
 			message: "remote sql execution is unavailable".into(),
@@ -46,10 +46,10 @@ fn remote_sql_response_exec() -> v6::ToEnvoy {
 	})
 }
 
-fn remote_sql_response_execute() -> v6::ToEnvoy {
-	v6::ToEnvoy::ToEnvoySqliteExecuteResponse(v6::ToEnvoySqliteExecuteResponse {
+fn remote_sql_response_execute() -> v8::ToEnvoy {
+	v8::ToEnvoy::ToEnvoySqliteExecuteResponse(v8::ToEnvoySqliteExecuteResponse {
 		request_id: 2,
-		data: v6::SqliteExecuteResponse::SqliteErrorResponse(v6::SqliteErrorResponse {
+		data: v8::SqliteExecuteResponse::SqliteErrorResponse(v8::SqliteErrorResponse {
 			group: "sqlite".into(),
 			code: "remote_unavailable".into(),
 			message: "remote sql execution is unavailable".into(),
@@ -57,27 +57,27 @@ fn remote_sql_response_execute() -> v6::ToEnvoy {
 	})
 }
 
-fn remote_sql_request_execute_batch() -> v6::ToRivet {
-	v6::ToRivet::ToRivetSqliteExecuteBatchRequest(v6::ToRivetSqliteExecuteBatchRequest {
+fn remote_sql_request_execute_batch() -> v8::ToRivet {
+	v8::ToRivet::ToRivetSqliteExecuteBatchRequest(v8::ToRivetSqliteExecuteBatchRequest {
 		request_id: 3,
-		data: v6::SqliteExecuteBatchRequest {
+		data: v8::SqliteExecuteBatchRequest {
 			namespace_id: "namespace".into(),
 			actor_id: "actor".into(),
 			generation: 7,
-			statements: vec![v6::SqliteBatchStatement {
+			statements: vec![v8::SqliteBatchStatement {
 				sql: "insert into t values (?)".into(),
-				params: Some(vec![v6::SqliteBindParam::SqliteValueInteger(
-					v6::SqliteValueInteger { value: 1 },
+				params: Some(vec![v8::SqliteBindParam::SqliteValueInteger(
+					v8::SqliteValueInteger { value: 1 },
 				)]),
 			}],
 		},
 	})
 }
 
-fn remote_sql_response_execute_batch() -> v6::ToEnvoy {
-	v6::ToEnvoy::ToEnvoySqliteExecuteBatchResponse(v6::ToEnvoySqliteExecuteBatchResponse {
+fn remote_sql_response_execute_batch() -> v8::ToEnvoy {
+	v8::ToEnvoy::ToEnvoySqliteExecuteBatchResponse(v8::ToEnvoySqliteExecuteBatchResponse {
 		request_id: 3,
-		data: v6::SqliteExecuteBatchResponse::SqliteExecuteBatchOk(v6::SqliteExecuteBatchOk {
+		data: v8::SqliteExecuteBatchResponse::SqliteExecuteBatchOk(v8::SqliteExecuteBatchOk {
 			results: Vec::new(),
 		}),
 	})
@@ -139,11 +139,11 @@ fn new_core_new_pegboard_envoy_allows_remote_sql_both_directions() -> Result<()>
 
 	assert!(matches!(
 		ToRivet::deserialize(&request, 4)?,
-		v6::ToRivet::ToRivetSqliteExecRequest(_)
+		v8::ToRivet::ToRivetSqliteExecRequest(_)
 	));
 	assert!(matches!(
 		ToEnvoy::deserialize(&response, 4)?,
-		v6::ToEnvoy::ToEnvoySqliteExecResponse(_)
+		v8::ToEnvoy::ToEnvoySqliteExecResponse(_)
 	));
 
 	Ok(())
@@ -235,11 +235,11 @@ fn remote_sql_batch_requires_v6() -> Result<()> {
 	let response = ToEnvoy::wrap_latest(remote_sql_response_execute_batch()).serialize(6)?;
 	assert!(matches!(
 		ToRivet::deserialize(&request, 6)?,
-		v6::ToRivet::ToRivetSqliteExecuteBatchRequest(_)
+		v8::ToRivet::ToRivetSqliteExecuteBatchRequest(_)
 	));
 	assert!(matches!(
 		ToEnvoy::deserialize(&response, 6)?,
-		v6::ToEnvoy::ToEnvoySqliteExecuteBatchResponse(_)
+		v8::ToEnvoy::ToEnvoySqliteExecuteBatchResponse(_)
 	));
 	Ok(())
 }